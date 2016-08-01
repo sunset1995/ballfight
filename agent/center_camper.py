3d285057ac681c1c925e2e12a22fa10d580512e7
Sunset <s2821d3721@gmail.com>
2016-08-01 17:17:52 +0800

    Update agent interface

--- agent/center_camper.py
+++ agent/center_camper.py
@@ -1,20 +1,8 @@
 import math
 
-def reaction(me, enemy, arena_r):
-    '''
-        the arena(circle) origin point is (0, 0)
-        left/right: (-/+, 0)
-        up/down: (0, -/+)
-        
-        all number below is float
-        me: [x, y, vx, vy]
-        enemy: [x, y, vx, vy]
-        arena_r: r
-    '''
-
-    ## your strategy
-    f = [-me[0], -me[1]]
+def reaction(myPos, mySpeed, enemyPos, enemySpeed, arenaR):
+    f = [-myPos[0], -myPos[1]]
     fLen = math.sqrt(f[0]**2 + f[1]**2)
-    f[0] = f[0]*500/fLen - me[2]
-    f[1] = f[1]*500/fLen - me[3]
+    f[0] = f[0]*500/fLen - mySpeed[0]
+    f[1] = f[1]*500/fLen - mySpeed[1]
     return f
--- agent/loser.py
+++ agent/loser.py
@@ -1,22 +1,10 @@
 import math
 
-def reaction(me, enemy, arena_r):
-    '''
-        the arena(circle) origin point is (0, 0)
-        left/right: (-/+, 0)
-        up/down: (0, -/+)
-        
-        all number below is float
-        me: [x, y, vx, vy]
-        enemy: [x, y, vx, vy]
-        arena_r: r
-    '''
-
-    ## your strategy
-    f = [-me[0], -me[1]]
+def reaction(myPos, mySpeed, enemyPos, enemySpeed, arenaR):
+    f = [-myPos[0], -myPos[1]]
     fLen = math.sqrt(f[0]**2 + f[1]**2)
     if math.fabs(fLen) < 0.1:
         return f
-    f[0] = f[0]*100/fLen - me[2]
-    f[1] = f[1]*100/fLen - me[3]
+    f[0] = f[0]*100/fLen - myPos[0]
+    f[1] = f[1]*100/fLen - myPos[1]
     return f
