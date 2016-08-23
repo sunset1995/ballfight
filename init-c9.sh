5414d6c02ca1a051cbc620499febb259d6504790
Sunset <s2821d3721@gmail.com>
2016-08-23 20:51:09 +0800

    Add c9 server build script

---
+++ init-c9.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver hkps.pool.sks-keyservers.net --recv D58C6920
+repo="trusty"
+. /etc/lsb-release
+if [ "$DISTRIB_RELEASE" == "16.04" ]; then
+    repo="xenial"
+fi
+echo "$DISTRIB_RELEASE"
+sleep 1
+sudo sh -c "echo 'deb http://package.crossbar.io/ubuntu $repo main' > /etc/apt/sources.list.d/crossbar.list"
+sudo apt-get update
+sudo apt-get install crossbar
+sudo pip3 install -r requirements.txt
+/opt/crossbar/bin/crossbar init
+cat .crossbar/config.json | sed 's/realm1/ballfight/g' > __crossbar.config
+mv __crossbar.config .crossbar/config.json
