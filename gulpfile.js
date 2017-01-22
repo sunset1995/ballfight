c34bb8c319b828ed3fbfb3fd5d2787e98ae0909d
Sunset <s2821d3721@gmail.com>
2017-01-22 11:02:43 +0800

    Remove unused component

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var babel = require('gulp-babel');
 
 
 
-gulp.task('default', ['main', 'ob']);
-gulp.task('debug', ['main-debug', 'ob-debug']);
+gulp.task('default', ['main']);
+gulp.task('debug', ['main-debug']);
 
 
 gulp.task('main', () => {
@@ -22,29 +22,6 @@ gulp.task('main', () => {
 
 gulp.task('main-debug', () => {
     return gulp.src('src/main.js')
-        .pipe(browserify({
-            debug: true,
-        }))
-        .pipe(babel({
-            presets: ['es2015'],
-            compact: false,
-        }))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('ob', () => {
-    return gulp.src('src/ob.js')
-        .pipe(browserify())
-        .pipe(babel({
-            presets: ['es2015'],
-            compact: true,
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('ob-debug', () => {
-    return gulp.src('src/ob.js')
         .pipe(browserify({
             debug: true,
         }))
